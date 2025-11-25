e74c3589c998bbf30bf61b5868bebfa7aba6e29e
arantxasampedro9-del <arantxasampedro9@gmail.com>
2025-11-25 17:57:37 +0100

    ca

--- main.c
+++ main.c
@@ -377,7 +377,7 @@ int main(void) {
             waitpid(h1, NULL, 0);
             waitpid(h2, NULL, 0);
         //TERCER PUNTO
-        }else if (line->ncommands > 2 && !line->background) {
+        }else if (line->ncommands > 2) {
 
             numComandos=line->ncommands;  //es cuántos comandos separa el parser según los |, calcula cuantos comandos hay
             // (type*)malloc(n_bytes), pero en C moderno no hace falta el tipo 
@@ -401,7 +401,7 @@ int main(void) {
                     }
             }   
               
-            //// se reserva memoria para los hijos, se guarda el PID de cada proceso hijo para luego hacer waitpid  
+            // se reserva memoria para los hijos, se guarda el PID de cada proceso hijo para luego hacer waitpid  
             hijos = malloc(numComandos * sizeof(pid_t));   
             if (hijos == NULL) {
                 perror("malloc hijos");
@@ -493,14 +493,31 @@ int main(void) {
                 close(tuberias[i][1]);
             }
 
-            // Esperar a TODOS los hijos, para que la minishell no imprima ==> antes de que acabe el pipeline
-            for (i = 0; i < numComandos; i++) {
-                waitpid(hijos[i], NULL, 0); //bloquea hasta que el hijo temrina
+            if (!line->background) {
+                // -------- FOREGROUND --------
+                for (i = 0; i < numComandos; i++) {
+                    waitpid(hijos[i], NULL, 0);
+                }
+            } else {
+            // -------- BACKGROUND --------
+                lista_bg[num_bg].pid = hijos[numComandos - 1];
+
+                strncpy(lista_bg[num_bg].comando, buf, sizeof(lista_bg[num_bg].comando));
+                lista_bg[num_bg].comando[sizeof(lista_bg[num_bg].comando)-1] = '\0';
+
+                lista_bg[num_bg].id = num_bg + 1;
+                num_bg++;
+
+                // Mensaje estilo bash
+                printf("[%d] %d\n", num_bg, hijos[numComandos - 1]);
             }
 
             //liberar memoria usada
             free(tuberias);
             free(hijos);
+            printf("==> ");
+            fflush(stdout);
+            continue;
         }
         printf("==> "); 
     }
