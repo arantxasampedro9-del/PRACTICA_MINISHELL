5f7130db04c497f28e542e51c9c20947c7dcad2d
fatimaallendegonzalez-source <fatimaallendegonzalez@gmail.com>
2025-12-22 12:30:32 +0100

    s

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdarg.h>
+
+int main(int argc, char *argv[]){
+    
+}
